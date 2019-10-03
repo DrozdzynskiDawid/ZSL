4c487428470fe7e928c3198dd5bc2e79bbd6a3cb
DrozdzynskiDawid <55097930+DrozdzynskiDawid@users.noreply.github.com>
2019-10-03 17:14:41 +0200

    Add files via upload

---
+++ dolaczanie_pliku.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    Początek pliku
+    <?php
+    /*
+    include './plik/name.php';
+    include_once './plik/name.php';
+*/
+
+    require './plik/name.php';
+    include './plik/name.php';
+
+
+
+
+     ?>
+     Koniec pliku
+  </body>
+</html>
