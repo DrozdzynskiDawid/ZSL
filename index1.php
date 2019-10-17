bb23388fe70c4f1d82c8a7158186319a11c2db58
Dawid Drożdżyński <55097930+DrozdzynskiDawid@users.noreply.github.com>
2019-10-17 18:15:14 +0200

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form  method="post">
+      <input type="text" name="imie"><br>
+      <input type="number" name="wiek">
+      <input type="submit" name="" value="ok">
+
+    </form>
+    <?php
+    if (!empty($_POST['imie'])) {
+      $imie = $_POST['imie'];
+      $dlugosc = strlen($imie);
+      // echo "Imie: $imie,dlugosc:",strlen($imie);
+      echo "Imie: $imie,dlugosc:$dlugosc<br>";
+      $imieN = ucfirst(strtolower(trim($imie)));
+      echo $imieN;
+    }
+    ?>
+  </body>
+</html>
